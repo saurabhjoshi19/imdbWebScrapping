a53a090a2a6a2f9e102ea3758f0d3fd0f5bd96fe
saurabhjoshi <saurabh.joshi190695@gmail.com>
2019-09-17 09:13:55 +0530

    main function added, removed unnecessary prints

--- webScrapper.py
+++ webScrapper.py
@@ -1,27 +1,12 @@
 import pandas as pd
 from requests import get
 from bs4 import BeautifulSoup
-from time import sleep
 from random import randint
-from IPython.core.display import clear_output
-from time import time
 
-start_time = time()
-requests = 0
-movies, years, ratings, metascores, votes = [],[],[],[],[]
-urls = ['https://www.imdb.com/search/title/?title_type=feature&release_date='+str(i) for i in range(2000,2020)]
-for url in urls:
+def webScrapper(url):
     response = get(url)
-    sleep(randint(8,15))
-
-    # Monitor the requests
-    requests += 1
-    elapsed_time = time() - start_time
-    print('Request:{}; Frequency: {} requests/s'.format(requests, requests/elapsed_time))
-    clear_output(wait = True)
     htmlSoup = BeautifulSoup(response.text, 'html.parser')
     movieContainers = htmlSoup.find_all('div',class_ = 'lister-item mode-advanced')
-
     for movieContainer in movieContainers:
         movies.append(movieContainer.h3.a.text)
         years.append(movieContainer.find('span', class_ = 'lister-item-year').text)
@@ -38,11 +23,17 @@ for url in urls:
         else:
             votes.append(int(0))
 
-dataframe = pd.DataFrame({'Id':range(1,len(movies)+1),
-                        'MovieName':movies,
-                        'ReleaseYear':years,
-                        'ImdbRating':ratings,
-                        'Metascore':metascores,
-                        'Votes':votes}).set_index('Id')
-
-dataframe.to_csv('imdbTop50From2000To2019.csv')
+if __name__ == '__main__':
+    movies, years, ratings, metascores, votes = [],[],[],[],[]
+    urls = ['https://www.imdb.com/search/title/?title_type=feature&release_date='+str(i) for i in range(2000,2004)]
+    for url in urls:
+        webScrapper(url)
+    dataframe = pd.DataFrame({'Id':range(1,len(movies)+1),
+                            'MovieName':movies,
+                            'ReleaseYear':years,
+                            'ImdbRating':ratings,
+                            'Metascore':metascores,
+                            'Votes':votes}).set_index('Id')
+    
+    filename = 'imdbtest'
+    dataframe.to_csv(filename)
